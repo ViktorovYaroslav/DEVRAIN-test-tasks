1544d91585e5197f401e300382c54008ab593767
Yaroslav Viktorov <viktorov.yaroslav@gmail.com>
2025-10-24 20:28:02 +0300

    Redesign message images.

--- src/features/tasks/_components/FullDemoChatView/components/UserMessage.tsx
+++ src/features/tasks/_components/FullDemoChatView/components/UserMessage.tsx
@@ -1,5 +1,3 @@
-import clsx from "clsx";
-
 import { MarkdownWrapper } from "@/components/ui/content";
 
 import type { FC } from "react";
@@ -13,22 +11,21 @@ interface Props {
 const UserMessage: FC<Props> = ({ content, images }) => {
 	return (
 		<article className="card__material ml-auto flex max-w-1/2 flex-col gap-3">
-			{content && <MarkdownWrapper>{content}</MarkdownWrapper>}
 			{images && images.length > 0 && (
-				<ul className="flex flex-wrap justify-end gap-2">
+				<ul className="-mx-3 -mt-3 flex flex-wrap gap-0.5">
 					{images.map(({ data, mediaType }, index) => (
-						<li key={`user-image-${index}`} className="shadow-sm">
+						<li key={`user-image-${index}`} className="overflow-hidden rounded-md">
 							<img
 								src={`data:${mediaType};base64,${data}`}
 								alt={`user upload ${index + 1}`}
-								className={clsx("max-h-36 rounded-md border border-white/20 object-cover", {
-									"w-36": !mediaType.startsWith("image/svg"),
-								})}
+								className="max-h-24 w-auto object-cover"
 							/>
 						</li>
 					))}
 				</ul>
 			)}
+
+			{content && <MarkdownWrapper>{content}</MarkdownWrapper>}
 		</article>
 	);
 };
